25c145a25c63384770a3e14b7be35643cc3a29f7
Yasunori Toshimitsu <yasu313nori@gmail.com>
2023-09-07 14:06:57 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import os
+
+root_dir = os.path.dirname(os.path.realpath(__file__))
+
+INSTALL_REQUIRES = [
+    "isaacgymenvs",
+    "wandb==0.12"
+]
+
+setup(
+    name="faive_gym",
+    author="Soft Robotics Lab",
+    version="0.0.1",
+    description="IsaacGym environments for the Faive Hand, intended to be used together with IsaacGymEnvs",
+    python_requires=">=3.8.*",
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages("."),
+)
